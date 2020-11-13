4e6ebd00332de13c70c186a447d3d14291210691
Jake Ketchum <ketchumjake@gmail.com>
2020-11-13 14:50:37 -0600

    fixed service / message typing issue

--- nodes/object_detection.py
+++ nodes/object_detection.py
@@ -19,16 +19,19 @@ TODO
 """
 
 import rospy
+import rospkg
 import sys
+import os
+import numpy as np
 import cv2 as cv
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Pose
 from sensor_msgs.msg import Image, CameraInfo, CompressedImage
 from can_sort.msg import Object
-from can_sort.srv import Board
+from can_sort.srv import Board, BoardResponse
 
 
-class detect():
+class Detect():
     """! A class for classifying bottles and cans for sorting by baxter.
     A holding class for the node's functions. See file level doc string for 
     more detailed information about the ROS API. 
@@ -38,13 +41,23 @@ class detect():
         self.can_diameter = 80       # [units are pixels]
         self.bottle_diameter = 60    # [units are pixels]
         
+
+        # Object Type Definitions
+        #TODO: Move these to a common library or param server.
+        self.ERROR = -1
+        self.BOTTLE = 0
+        self.CAN = 1
+        
         # Info for default image 
-        self.image_directory ="../camera_images/11.10.20/"
+        self.rospack = rospkg.RosPack()
+        path = self.rospack.get_path(name = 'can_sort') + '/camera_images/11.10.20/'
+        self.image_directory = path
         self.image_name = "bottle_top_1.jpg"
     
         #TODO: figure oout correct commenting style 
         ##! Stores the current image. 
-        self.img = self.set_default_image()
+        self.set_default_image()
+        
         #self.setup_image_stream()
 
     def setup_image_stream(self):
@@ -65,16 +78,13 @@ class detect():
         prevent invalid service calls.
         """
         rospy.loginfo("Setting up services")
-        s = rospy.service('get_board_state', Board, handle_board_state)
-        pass
+        s = rospy.Service('get_board_state', Board, self.get_board_state)
 
     def set_default_image(self):
         """! Load a default image from the local files. Used for testing. 
         """  
-        
         # Read in the image
         self.img = cv.imread(self.image_directory + self.image_name)
-
         # Check the file name was right
         if self.img is None: 
             rospy.logwarn("WARNING: could not read default image")
@@ -97,65 +107,114 @@ class detect():
         #TODO: store centrally or remove.
         (rows,cols,channels) = cv_image.shape
 
-    def handle_board_state(self, srv):
+    def get_board_state(self, srv):
         """! Run object detection to produce board state from last stored image.
         
         """
+
+        # This is just the script for testing, evertying here needs to change
         # Set local so no change during run
         img = self.img
-        paint_image= paint_circles(img, img, (0, 0, 255), 25)
-
-
+        
+        objects = [] 
+        objects.extend(self.detect_cans(img))
+        objects.extend(self.detect_bottles(img))
+        print(objects )
+        
+    
+        #(circles, paint_image) = self.paint_circles(self.img, img, (0, 0, 255), 25)
         # Find bottles
-        paint_image= paint_circles(img, paint_image, (255, 0, 0), 20)
+        #(circles, paint_image) = self.paint_circles(img, paint_image, (255, 0, 0), 20)
 
-        # Find bottle tabs
-        paint_image= paint_circles(img, paint_image, (0, 255, 0), 20, max_rad = 10)
 
-        remove_table(img)
-        cv.imshow("detected circles",paint_image )
-        return []
+        #cv.imshow("detected circles",paint_image )
+        #cv.waitKey(0)
+
+        return  BoardResponse(objects)
       
 
-
-    
-    def classify_objects(self):
+    def detect_cans(self, image):
+        """!
         """
-        TODO - Documentaio/n
+        rospy.loginfo("Detecting Cans")
+        circles = self.detect_circles(image, min_rad = 25, max_rad = 30 )
+        cans = []
+        for c in circles[0]:
+            can = Object()
+            can.type = self.CAN
+            can.sorted = False 
+            can.location.x = c[0]
+            can.location.y = c[1]
+            can.location.z = -1 #TODO: Impliment a decent vertical offset 
+            cans.append(can)
+        for c in circles[0]:
+            can = Object()
+            can.type = self.CAN
+            can.sorted = False 
+            can.location.x = c[0]
+            can.location.y = c[1]
+            can.location.z = -1 #TODO: Impliment a decent vertical offset 
+            cans.append(can)
+        return(cans)
+
+    def detect_bottles(self, image):
+        """!
         """
-        # TODO
-        pass
+        rospy.loginfo("Detecting Cans")
+        circles = self.detect_circles(image, min_rad = 20, max_rad = 25 )
+        cans = []
+        for c in circles[0]:
+            can = Object()
+            can.type = self.BOTTLE
+            can.sorted = False 
+            can.location.x = c[0]
+            can.location.y = c[1]
+            can.location.z = -1 #TODO: Impliment a decent vertical offset 
+            cans.append(can)
+        return(cans)
 
+    def detect_circles(self, image, min_rad = 20, max_rad = 30):
+        """!
+        """
+        # Go to greyscale   
+        grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
+        # Blur the image, this was in the tutorial who knows why
+        grey = cv.medianBlur(grey, 5)
+        
+        # Run the algorithm, get the circles
+        rows = grey.shape[0]
+        circles = cv.HoughCircles(grey, cv.HOUGH_GRADIENT, 1, rows / 8, 
+                                  param1 = 100, param2 = 30,
+                                  minRadius = min_rad, maxRadius = max_rad)
+        return(circles) 
 
-def paint_circles(image, paint_image, color, min_rad, max_rad = 30):
-    """! This function finds all the specified circles and paints them.
-    """ 
-    # TODO: Commeint or delete
-    
-    # Go to greyscale   
-    grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    def paint_circles(self, image, paint_image, color, min_rad = 20, max_rad = 30):
+        """! This function finds all the specified circles and paints them.
+        """ 
+        # TODO: Commeint or delete
+        
+        # Go to greyscale   
+        grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-    # Blur the image, this was in the tutorial who knows why
-    grey = cv.medianBlur(grey, 5)
-    
-    # Run the algorithm, get the circles
-    rows = grey.shape[0]
-    circles = cv.HoughCircles(grey, cv.HOUGH_GRADIENT, 1, rows / 8, 
-                              param1 = 100, param2 = 30,
-                              minRadius = min_rad, maxRadius = max_rad)
-    
-    # Paint the circles onto our paint image
-    if circles is not None: 
-        circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-            print (i)
-            center = (i[0], i[1])
-            cv.circle(paint_image, center, 1, (0, 100, 100), 3)
-            radius = i[2]
-            cv.circle(paint_image, center, radius, color, 3)
+        # Blur the image, this was in the tutorial who knows why
+        grey = cv.medianBlur(grey, 5)
+        
+        # Run the algorithm, get the circles
+        rows = grey.shape[0]
+        circles = cv.HoughCircles(grey, cv.HOUGH_GRADIENT, 1, rows / 8, 
+                                  param1 = 100, param2 = 30,
+                                  minRadius = min_rad, maxRadius = max_rad)
+        # Paint the circles onto our paint image
+        if circles is not None: 
+            circles = np.uint16(np.around(circles))
+            for i in circles[0,:]:
+                center = (i[0], i[1])
+                cv.circle(paint_image, center, 1, (0, 100, 100), 3)
+                radius = i[2]
+                cv.circle(paint_image, center, radius, color, 3)
 
-    return(paint_image)
+        return(circles,paint_image)
 
 
 
@@ -164,12 +223,13 @@ def paint_circles(image, paint_image, color, min_rad, max_rad = 30):
 
 def main():
     """ The main() function """
-    rospy.init_node('object detection', log_level = rospy.DEBUG)
+    rospy.init_node('object_detection', log_level = rospy.DEBUG)
     rospy.logdebug(f"classification node started")
-    classification = Classification()
-     
+    detect = Detect()
+    detect.setup_services()
+    rospy.spin()
 
-    #cv.destroyAllWindows()
+    cv.destroyAllWindows()
 
 
 if __name__ == '__main__':
