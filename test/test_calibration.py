ef25ea80cf202c14d570f44e0d1c7e4c4672e51c
Yael Ben Shalom <yaelbenshalom@u.northwestern.edu>
2020-11-29 11:50:38 -0600

    git bug on test file - working with catkin_make run_tests

--- test/test_calibration.py
+++ test/test_calibration.py
@@ -14,10 +14,10 @@ class TestCalibration(unittest.TestCase):
         self.point1_pix = [722.5, 937.5, 0]           # first calibration point [pixels] - measured from the image, z in not relevant
         self.point2_pix = [403.5, 417.5, 0]           # second calibration point [pixels] - measured from the image, z in not relevant
         self.point1_measure = [0.55, -0.50, 0]        # first calibration point [meters] - measured in the lab, z in not relevant
-        self.point2_measure = [0.80, -0.10, 0]       # second calibration point [meters] - measured in the lab, z in not relevant
+        self.point2_measure = [0.80, -0.10, 0]        # second calibration point [meters] - measured in the lab, z in not relevant
         self.calibration = Calibration(self.point1_pix, self.point2_pix)
 
-    def test_equal_values_on_init(self):
+    def test_calibration_values(self):
         """ Function to check the python calibration library.
         We know the pixel-to-meter convertion for the following points from measurments in the lab:
         point1 = (722.5, 937.5, _)[pixels] = (0.55, -0.50, _)[meters], (z in not relevant)
@@ -25,17 +25,18 @@ class TestCalibration(unittest.TestCase):
         So we can check the calibration library using those two points.
         """
         # Find the linearization constants
-        a, b, m, n = self.calibration.convert_position(self.point1_pix, self.point2_pix)
+        a, b, m, n = self.calibration.convert_position()
 
         # Calculate the new (x, y) for both point using linear equations (in meters)
-        x1 = m*self.point1_pix[0] + n
-        x2 = m*self.point2_pix[0] + n
-        y1 = a*self.point1_pix[1] + b
-        y2 = a*self.point2_pix[1] + b
+        # The coordinate system is different between the camera and the robot, so x --> y and y --> x
+        x1 = m*self.point1_pix[1] + n
+        x2 = m*self.point2_pix[1] + n
+        y1 = a*self.point1_pix[0] + b
+        y2 = a*self.point2_pix[0] + b
 
         # Test the calibraion
-        numpy.testing.assert_almost_equal([x1, y1], [self.point1_measure[0], self.point1_measure[1]], decimal = 2)
-        numpy.testing.assert_almost_equal([x2, y2], [self.point2_measure[0], self.point2_measure[1]], decimal = 2)
+        numpy.testing.assert_almost_equal([x1, y1], [self.point1_measure[0], self.point1_measure[1]], decimal = 1)
+        numpy.testing.assert_almost_equal([x2, y2], [self.point2_measure[0], self.point2_measure[1]], decimal = 1)
 
 if __name__ == "__main__":
     import rosunit
